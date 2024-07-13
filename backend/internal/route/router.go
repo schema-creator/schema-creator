3d777755c6e728443ccd41caa22435dc64373a15
sikigasa <km3t1114@yahoo.co.jp>
2024-07-13 16:09:16 +0900

    add: google route

--- backend/internal/route/router.go
+++ backend/internal/route/router.go
@@ -5,6 +5,10 @@ import (
 
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
+	"github.com/schema-creator/schema-creator/schema-creator/internal/adapter/controller"
+	"github.com/schema-creator/schema-creator/schema-creator/internal/container"
+	"github.com/schema-creator/schema-creator/schema-creator/internal/framework/cookie"
+	"github.com/schema-creator/schema-creator/schema-creator/internal/usecase/interactor"
 )
 
 type router struct {
@@ -35,4 +39,12 @@ func (r *router) health() {
 	r.echo.GET("/healthz", func(c echo.Context) error {
 		return c.String(http.StatusOK, `{"status:":"ok"}`)
 	})
+}
+
+func (r *router) GoogleLogin(corsRoute *echo.Group) {
+	googleLogin := container.Invoke[*interactor.GoogleLogin]()
+	cookieSetter := container.Invoke[*cookie.CookieSetter]()
+
+	corsRoute.POST("/login/google", controller.GoogleLogin(googleLogin, cookieSetter))
+
 }

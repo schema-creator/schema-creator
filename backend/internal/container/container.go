7f20e4c8750fac04fa7d1eff3e9129cae6c533ce
sikigasa <km3t1114@yahoo.co.jp>
2024-07-17 18:40:28 +0900

    add: auth

--- backend/internal/container/container.go
+++ backend/internal/container/container.go
@@ -1,10 +1,13 @@
 package container
 
 import (
+	"github.com/schema-creator/schema-creator/schema-creator/internal/adapter/gateway/authz"
 	"github.com/schema-creator/schema-creator/schema-creator/internal/adapter/gateway/authz/github"
 	"github.com/schema-creator/schema-creator/schema-creator/internal/adapter/gateway/authz/google"
+	"github.com/schema-creator/schema-creator/schema-creator/internal/adapter/gateway/repository"
 	"github.com/schema-creator/schema-creator/schema-creator/internal/driver/db"
 	"github.com/schema-creator/schema-creator/schema-creator/internal/framework/cookie"
+	"github.com/schema-creator/schema-creator/schema-creator/internal/usecase/dai"
 	"github.com/schema-creator/schema-creator/schema-creator/internal/usecase/interactor"
 	"go.uber.org/dig"
 )
@@ -20,13 +23,16 @@ func NewContainer() error {
 	container = dig.New()
 
 	args := []provideArg{
+		{constructor: google.DefaultGoogleOAuth2Config, opts: []dig.ProvideOption{}},
+		{constructor: github.DefaultGitHubOAuth2Config, opts: []dig.ProvideOption{}},
 		{constructor: db.Connect, opts: []dig.ProvideOption{}},
 		{constructor: db.NewGORM, opts: []dig.ProvideOption{}},
 		{constructor: cookie.DefaultCookieOptions, opts: []dig.ProvideOption{}},
 		{constructor: cookie.NewCookieSetter, opts: []dig.ProvideOption{}},
-		{constructor: google.NewOAuth, opts: []dig.ProvideOption{}},
-		{constructor: github.NewOAuth, opts: []dig.ProvideOption{}},
+		{constructor: google.NewGoogleOAuth, opts: []dig.ProvideOption{dig.As(new(authz.GoogleOAuth2))}},
+		{constructor: github.NewGitHubOAuth, opts: []dig.ProvideOption{dig.As(new(authz.GitHubOAuth2))}},
 		{constructor: interactor.NewGoogleLogin, opts: []dig.ProvideOption{}},
+		{constructor: repository.NewGormRepo, opts: []dig.ProvideOption{dig.As(new(dai.DataAccessInterfaces))}},
 		{constructor: interactor.NewGitHubLogin, opts: []dig.ProvideOption{}},
 		{constructor: interactor.NewSessionInteractor, opts: []dig.ProvideOption{}},
 	}
